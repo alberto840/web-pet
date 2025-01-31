b1ffdf47b395b68b8026673c1557cb778c389123
Alberto Flores <alberto.flores@ucb.edu.bo>
2025-01-31 01:55:20 -0400

    enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "http://pet-veteriana-backend-production.up.railway.app/"
+  apiUrl: "https://pet-veteriana-backend-production.up.railway.app/"
 };
