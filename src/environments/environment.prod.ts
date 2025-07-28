64842cf7211a5a998d1a057ba8051c43f43062d1
Alberto Flores <alberto.flores@ucb.edu.bo>
2025-07-28 10:56:20 -0400

    cambiando a render

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "https://backend-petwise-production.up.railway.app/",
+  apiUrl: "https://backend-petwise.onrender.com/",
   recaptcha: "6LdU0HgrAAAAAPrbydOLWLXbEc452Tsn5Cp4lLdL",
 };
